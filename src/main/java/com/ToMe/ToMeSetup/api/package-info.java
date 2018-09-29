14ba0a228f6040b544428a703f4c41aca5c34b3b
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2018-09-29 16:22:14 +0200

    Updated to 1.5
    * Recoded Start Item System to use a new API making it much more easy to create addons.
    * Added a startitems Command to give Players any time Start Items
    * Upload grade.properties

---
+++ src/main/java/com/ToMe/ToMeSetup/api/package-info.java
@@ -0,0 +1,6 @@
+//@API(apiVersion=ToMeSetupMod.API_VERSION,owner=ToMeSetupMod.API_OWNER, provides=ToMeSetupMod.API_PROVIDES)
+@API(apiVersion=ApiProps.VERSION,owner=ApiProps.OWNER, provides=ApiProps.PROVIDES)
+package com.ToMe.ToMeSetup.api;
+
+import net.minecraftforge.fml.common.API;
+//import com.ToMe.ToMeSetup.ToMeSetupMod;
